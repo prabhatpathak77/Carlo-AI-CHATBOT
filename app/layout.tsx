9765f8f07556ecfbc54633f577c97c519b2ff17b
prabhatpathak77 <gargprabhat93@gmail.com>
2025-03-18 19:28:47 +0530

    added anylytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { cn } from "@/lib/utils"
 import { TooltipProvider } from "@/components/ui/tooltip"
 import { Inter } from "next/font/google"
 import type { ReactNode } from "react"
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -15,6 +16,7 @@ export default function Layout({ children }: { children: ReactNode }) {
     <html lang="en">
       <body className={cn("flex min-h-svh flex-col antialiased", inter.className)}>
         <TooltipProvider delayDuration={0}>{children}</TooltipProvider>
+        <Analytics />
       </body>
     </html>
   )
