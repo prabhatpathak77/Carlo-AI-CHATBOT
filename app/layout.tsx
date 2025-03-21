452de3f82fb007a9d5505be4c54602a747c0fd7b
prabhatpathak77 <gargprabhat93@gmail.com>
2025-03-21 14:29:18 +0530

    made some chnages in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
   title: "Carlo",
-  description: "Your intelligent conversation partner made by Prabhat.",
+  description: "Your intelligent conversation partner made by Prabhat & Prachi.",
 }
 
 export default function Layout({ children }: { children: ReactNode }) {
